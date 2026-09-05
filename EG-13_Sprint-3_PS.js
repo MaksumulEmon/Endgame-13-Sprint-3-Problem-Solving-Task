f65fd19025f55781858fad53940731924cf44e59
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-05 21:47:55 +0600

    Initial commit - 3Sum

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -65,7 +65,7 @@ console.log(canConstruct("aa", "aab"));
 
 
 // 05. Majority Element
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     let count = {};
 
     for (let num of nums) {
@@ -80,3 +80,38 @@ var majorityElement = function(nums) {
 const nums1 = [2, 2, 1, 1, 1, 2, 2];
 
 console.log(majorityElement(nums1));
+
+
+
+// 06. 3Sum
+
+var threeSum = function (nums) {
+    let result = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1, r = nums.length - 1;
+
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+
+                while (l < r && nums[l] === nums[l - 1]) l++;
+            } else if (sum < 0) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+
+    return result;
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
