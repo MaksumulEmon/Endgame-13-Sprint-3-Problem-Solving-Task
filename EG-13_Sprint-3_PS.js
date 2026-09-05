e69c353e5aa46df290e39ce30af66794d536d7bb
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-05 19:49:49 +0600

    Initial commit - Move Zeroes

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -5,3 +5,29 @@ var containsDuplicate = function(nums) {
 };
 
 console.log(containsDuplicate([1, 2, 3]));
+
+
+
+
+// 02. Move Zeroes
+var moveZeroes = function(nums) {
+    let index = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    while (index < nums.length) {
+        nums[index] = 0;
+        index++;
+    }
+};
+
+let nums = [0, 1, 0, 3, 12];
+
+moveZeroes(nums);
+
+console.log(nums);
