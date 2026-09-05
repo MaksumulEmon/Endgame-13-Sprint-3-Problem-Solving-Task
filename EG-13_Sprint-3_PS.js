fb12f580b15eaa30a79936e13c4549e533094d24
MaksumulEmon <maksumulemon300@gmail.com>
2026-09-06 00:44:58 +0600

    Initial commit -  Longest Consecutive Sequence

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -141,7 +141,7 @@ console.log(subarraySum([1, 1, 1], 2));
 
 // 08. Top K Frequent Elements
 
-var topKFrequent = function(nums, k) {
+var topKFrequent = function (nums, k) {
     const frequency = {};
 
     // Count frequency
@@ -158,3 +158,32 @@ var topKFrequent = function(nums, k) {
     return sorted.slice(0, k).map(Number);
 };
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+
+
+
+
+
+// 09. Longest Consecutive Sequence
+var longestConsecutive = function (nums) {
+    const set = new Set(nums);
+    let longest = 0;
+
+    for (const num of set) {
+        // Start only if num is the beginning of a sequence
+        if (!set.has(num - 1)) {
+            let current = num;
+            let length = 1;
+
+            while (set.has(current + 1)) {
+                current++;
+                length++;
+            }
+
+            longest = Math.max(longest, length);
+        }
+    }
+
+    return longest;
+};
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
